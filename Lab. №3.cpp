57839326ba2c42fe25c836a141d9998af69ad9cf
i888844 <i888844@yandex.ru>
2023-10-27 19:51:03 +0300

    3rd commit

--- Lab. №3.cpp
+++ Lab. №3.cpp
@@ -73,7 +73,7 @@ public:
 	}
 	int get_track_number() const { return track_number; }
 	int get_max_people_amount() const { return max_people_amount; }
-	int max_people_amount() const { return people_amount; }
+	int get_people_amount() const { return people_amount; }
 	void set_track_number(int _settable_track_number)
 	{
 		if (_settable_track_number >= 0)
@@ -108,9 +108,145 @@ public:
 class swimming_pool
 {
 private:
-
+	int swimming_pool_number;
+	int max_childrens_tracks_amount;
+	int childrens_tracks_amount;
+	childrens_track** childrens_tracks;
+	int max_adults_tracks_amount;
+	int adults_tracks_amount;
+	adults_track** adults_tracks;
+	double max_depth;
+	double lenght;
 public:
-
+	swimming_pool()
+	{
+		int i = 0;
+		swimming_pool_number = 0;
+		max_childrens_tracks_amount = 2;
+		childrens_tracks_amount = 2;
+		childrens_tracks = new childrens_track * [max_childrens_tracks_amount];
+		for (i = 0; i < max_childrens_tracks_amount; i++)
+		{
+			childrens_tracks[i] = new childrens_track(i + 1, 10, 0);
+		}
+		max_adults_tracks_amount = 3;
+		adults_tracks_amount = 3;
+		adults_tracks = new adults_track * [max_adults_tracks_amount];
+		for (i = 0; i < max_adults_tracks_amount; i++)
+		{
+			adults_tracks[i] - new adults_track(i + 1, 10, 0);
+		}
+		max_depth = 5.0;
+		lenght = 25.0;
+	}
+	swimming_pool(int _swimming_pool_number, int _max_childrens_tracks_amount, int _childrens_tracks_amount, int _max_adults_tracks_amount, int _adults_tracks_amount, double _max_depth, double _lenght)
+	{
+		int i = 0;
+		if (_swimming_pool_number >= 0)
+		{
+			swimming_pool_number = _swimming_pool_number;
+		}
+		else
+		{
+			swimming_pool_number = 0;
+		}
+		if (_max_childrens_tracks_amount >= 1)
+		{
+			max_childrens_tracks_amount = _max_childrens_tracks_amount;
+		}
+		else
+		{
+			max_childrens_tracks_amount = 2;
+		}
+		if (_childrens_tracks_amount <= _max_childrens_tracks_amount && _childrens_tracks_amount >= 0)
+		{
+			childrens_tracks_amount = _childrens_tracks_amount;
+		}
+		else
+		{
+			childrens_tracks_amount = 0;
+		}
+		childrens_tracks = new childrens_track * [max_childrens_tracks_amount];
+		for (i = 0; i < max_childrens_tracks_amount; i++)
+		{
+			childrens_tracks[i] = new childrens_track(i + 1, 10, 0);
+		}
+		if (_max_adults_tracks_amount >= 1)
+		{
+			max_adults_tracks_amount = _max_adults_tracks_amount;
+		}
+		else
+		{
+			max_adults_tracks_amount = 3;
+		}
+		if (_adults_tracks_amount <= _max_adults_tracks_amount && _adults_tracks_amount >= 0)
+		{
+			adults_tracks_amount = _adults_tracks_amount;
+		}
+		else
+		{
+			adults_tracks_amount = 3;
+		}
+		adults_tracks = new adults_track * [max_adults_tracks_amount];
+		for (i = 0; i < max_adults_tracks_amount; i++)
+		{
+			adults_tracks[i] = new adults_track(i + 1, 10, 0);
+		}
+	}
+	swimming_pool(swimming_pool& src_object)
+	{
+		int i = 0;
+		int j = 0;
+		swimming_pool_number = src_object.swimming_pool_number;
+		max_childrens_tracks_amount = src_object.max_childrens_tracks_amount;
+		childrens_tracks_amount = src_object.childrens_tracks_amount;
+		for (i = 0; i < max_childrens_tracks_amount; i++)
+		{
+			childrens_tracks[i] = new childrens_track(
+				src_object.childrens_tracks[i]->get_track_number(),
+				src_object.childrens_tracks[i]->get_max_people_amount(),
+				src_object.childrens_tracks[i]->get_people_amount());
+			if (src_object.childrens_tracks[i]->get_people_amount() > 0)
+			{
+				for (j = 0; j < src_object.childrens_tracks[i]->get_people_amount(); j++)
+				{
+					// childrens_tracks[i]->add_human_age(src_object.childrens_tracks[i]->get_human_age_by_index(i));
+				}
+			}
+		}
+		max_adults_tracks_amount = src_object.max_adults_tracks_amount;
+		adults_tracks_amount = src_object.adults_tracks_amount;
+		for (i = 0; i < max_adults_tracks_amount; i++)
+		{
+			adults_tracks[i] = new adults_track(
+				src_object.adults_tracks[i]->get_track_number(),
+				src_object.adults_tracks[i]->get_max_people_amount(),
+				src_object.adults_tracks[i]->get_people_amount());
+			if (src_object.adults_tracks[i]->get_people_amount() > 0)
+			{
+				for (j = 0; j < src_object.adults_tracks[i]->get_people_amount(); j++)
+				{
+					// adults_tracks[i]->add_human_age(src_object.adults_tracks[i]->get_human_age_by_index(i));
+				}
+			}
+		}
+		max_depth = src_object.max_depth;
+		lenght = src_object.lenght;
+	}
+	~swimming_pool()
+	{
+		int i = 0;
+		for (i = 0; i < max_childrens_tracks_amount; i++)
+		{
+			delete childrens_tracks[i];
+		}
+		delete[]childrens_tracks;
+		for (i = 0; i < max_adults_tracks_amount; i++)
+		{
+			delete adults_tracks[i];
+		}
+		delete[]adults_tracks;
+	}
 };
 
 class sport_complex
